1cd5a4833c61c9bb0c3adbe840de0d7370b4c5fa
annavilner <annavilner@gmail.com>
2026-01-11 22:05:02 +0000

    removeProductFromCart(productName: string);

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -68,3 +68,24 @@ test ('remove product from cart', async ({ page }) => {
 
 
 });
+test('validate cart is empty after removing all products', async ({ page }) => {
+  const login = new LoginPage(page);
+  const products = new ProductsPage(page);
+  const cart = new CartPage(page);
+
+  await login.goto();
+  await login.login('standard_user', 'secret_sauce');
+
+  await products.addProductToCart('Sauce Labs Backpack');
+  await products.expectCartCount('1');
+ await products.goToCart();
+  await products.removeProductFromCart('Sauce Labs Backpack');
+  await products.expectCartCount('0');
+  const removedProduct = page.locator(
+    '.inventory_item_name',
+    { hasText: 'Sauce Labs Backpack' }
+  );
+  
+ 
+  await expect(page.getByText('Sauce Labs Backpack')).toHaveCount(0);
+});
