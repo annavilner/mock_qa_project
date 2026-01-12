1156d2306247804291b4456f4dc075b336a1c84d
annavilner <annavilner@gmail.com>
2026-01-12 22:43:50 +0000

    Your commit message

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('GET /api/users/2 - validate response', async ({ request }) => {
+test('GET /api/ validate response', async ({ request }) => {
   const response = await request.get('https://f6b2a073-3ae1-447f-9f7e-a9eb76502794.mock.pstmn.io/accounts/{account_id}/balance', {
     headers: {
       'User-Agent': 'Playwright API Test',
@@ -10,12 +10,16 @@ test('GET /api/users/2 - validate response', async ({ request }) => {
 
   // Validate status code
   expect(response.status()).toBe(200);
-
+ expect(response.ok()).toBeTruthy();
   const responseBody = await response.json();
 
   // Validate response fields
-  expect(responseBody.account_id).toBeDefined();
-  
-  expect(responseBody.account.first_name).toBeDefined();
+  expect(responseBody).toHaveProperty('account_id');
+  expect(responseBody).toHaveProperty('balance');
+
+  // ✅ Field values & types
+  expect(responseBody.account_id).toBe('ACC001');
+  expect(typeof responseBody.balance).toBe('number');
+
 });
 
